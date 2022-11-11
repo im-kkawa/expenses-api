41d9eb665a1a3fe896a996cd1b3ef0d5394da924
kkawamura <im.kkawamura@gmail.com>
2022-11-11 10:36:39 +0900

    [db]create_expenses_table作成

---
+++ db/migrations/20221111011600_create_expenses_table.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('expenses', function (table) {
+    table.increments('id').primary(); // Set this column as the primary key
+    table.date('date').notNullable();
+    table.string('item', 32).notNullable();
+    table.string('note', 32);
+    table.bigint('deposit');
+    table.bigint('withdrawal');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  knex.schema.dropTable('expenses');
+};
